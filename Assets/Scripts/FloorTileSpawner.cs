63d8b3dbb9156c3855cbd7db547b2b4c40ccff48
kinasch <janik@schoenberg.name>
2021-08-06 22:49:55 +0200

    Random Tile Spawner Doen

--- Assets/Scripts/FloorTileSpawner.cs
+++ Assets/Scripts/FloorTileSpawner.cs
@@ -13,7 +13,9 @@ public class FloorTileSpawner : MonoBehaviour
     [SerializeField] private int seed;
 
     private List<float> streetRows = new List<float>();
+    private Dictionary<float, GameObject[]> tiles = new Dictionary<float, GameObject[]>();
     private int rows = 0;
+    private bool startUpDone = false;
     
     void Start()
     {
@@ -24,14 +26,17 @@ public class FloorTileSpawner : MonoBehaviour
         SpawnTiles();
         // Second, "remove" the loading screen.
         loadingScreen.gameObject.SetActive(false);
+        startUpDone = true;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (false/*rows < mainCam.position.x + 12.1f*/)
+        if (rows < (Math.Round(mainCam.position.x) + 20f) && startUpDone)
         {
-            SpawnRandomTiles(mainCam.position.x+12.6f);
+            SpawnRandomTiles(rows-8.5f);
         }
+        
+        /*TileGarbageCollector(mainCam.position.x-12);*/
     }
 
     private void SpawnTiles()
@@ -52,26 +57,44 @@ public class FloorTileSpawner : MonoBehaviour
 
     private void SpawnRandomTiles(float xRow, float randomizer = 10f)
     {
+        List<GameObject> newObjectsList = new List<GameObject>();
         randomizer = randomizer > 1 ? Random.value : randomizer;
         for (float j = -5.5f; j <= 5.5f; j++)
         {
             if (randomizer < 0.7f)
             {
                 var newObj = Instantiate(floorTilePrefab, new Vector3(xRow, j, 0), new Quaternion(), parents[0].transform);
+                newObjectsList.Add(newObj);
             }
             else if (randomizer > 0.7f && randomizer < 0.9f)
             {
                 var newObj = Instantiate(floorTilePrefab, new Vector3(xRow, j, 0), new Quaternion(), parents[1].transform);
                 newObj.GetComponent<SpriteRenderer>().color = Color.black;
-                if(j > 5) streetRows.Add(xRow);
+                if (j > 5)
+                {
+                    streetRows.Add(xRow);
+                }
+                newObjectsList.Add(newObj);
             }
             else if (randomizer > 0.9f)
             {
                 var newObj = Instantiate(floorTilePrefab, new Vector3(xRow, j, 0), new Quaternion(), parents[2].transform);
                 newObj.GetComponent<SpriteRenderer>().color = Color.red;
+                newObjectsList.Add(newObj);
             }
         }
-
+        tiles.Add(xRow, newObjectsList.ToArray());
         rows++;
+    }
+
+    private void TileGarbageCollector(float xPos)
+    {
+        var list = tiles[xPos];
+        foreach (var gO in list)
+        {
+            Destroy(gO);
+        }
+
+        tiles.Remove(xPos);
     }
 }
